f0d9fb3bcd36915e087a8188a634a9b04f3d95a3
RiverKingPrograming <stewartm.justin@outlook.com>
2024-03-14 21:49:24 -0400

    Added acending order to returned events in database.js

--- Server/database.js
+++ Server/database.js
@@ -33,7 +33,7 @@ export async function createEvent(Name, day) {
 }
 
 export async function getWeeks(X, Y) {
-  const [rows] = await pool.query('SELECT * FROM events WHERE day < (?) AND day > (?)', [X, Y])
+  const [rows] = await pool.query('SELECT * FROM events WHERE day < (?) AND day > (?) ORDER BY day ASC', [X, Y])
   return rows
 }
 
