37e6b05ea66f69554539be7725c6a52789051a5d
stewartmjustin <stewartm.justin@outlook.com>
2024-03-22 13:08:33 -0400

    Added a show all events checkbox thats functional and uses react components and a use state hook

--- Client/src/Viewer.jsx
+++ Client/src/Viewer.jsx
@@ -3,9 +3,19 @@ import Event from "./Event"
 
 function Viewer() {
     const [weeksData, setWeeks] = useState([{}])
-    const [xEvents, setXEvents] = useState(0)
+    const [allEvents, setAllEvents] = useState(false)
     function getNextWeeks(X) {
         fetch("http://localhost:3000/events/weeks/" + X).then(
+            response => response.json()
+        ).then(
+            data => {
+                setWeeks(data)
+            }
+        )
+    }
+
+    function getEvents() {
+        fetch("http://localhost:3000/events").then(
             response => response.json()
         ).then(
             data => {
@@ -17,14 +27,24 @@ function Viewer() {
     useEffect(() => {
         getNextWeeks(2)
     }, [])
+
     return ( 
         <>
             <h1>Viewer</h1>
-            {/*<label htmlFor="allEventsCheck">Show All Events: </label>
-            <input type="checkbox" id="allEventsCheck" /><br />*/}
-            <label htmlFor="weeksNumber">See Events for how many weeks ahead?</label>
-            <input type="number" id="weeksNumber" defaultValue={2} onChange={() => {getNextWeeks(document.getElementById('weeksNumber').value)}}></input>
-            <button type="button" className="good" id="ViewBtn" onClick={() => {getNextWeeks(document.getElementById('weeksNumber').value)}}>Submit</button>
+            <div className="inline">
+                <label htmlFor="allEventsCheck">Show All Events: </label>
+                <input type="checkbox" id="allEventsCheck" onClick={() => {setAllEvents(document.getElementById("allEventsCheck").checked);  /*void(!allEvents && getEvents())*/(allEvents) ? (getNextWeeks(2)): (getEvents())}}/>
+            </div>
+            {(allEvents) ? (
+                <button type="button" className="good" id="ViewBtn">Update!</button>
+            ): (
+                <>
+                    <label htmlFor="weeksNumber">See Events for how many weeks ahead?</label>
+                    <input type="number" id="weeksNumber" defaultValue={2} onChange={() => {getNextWeeks(document.getElementById('weeksNumber').value)}}></input>
+                    <button type="button" className="good" id="ViewBtn" onClick={() => {getNextWeeks(document.getElementById('weeksNumber').value)}}>Submit</button>
+                </>
+            )}
+            
             <div className="isEvents">
                 {(JSON.stringify(weeksData) === '[]') ? (
                     <h1>No Events Found</h1>
